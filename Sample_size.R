4248e621bc174fe442387bbbf579e1aaaf7541b2
My-Luong Vuong <myluong.vuong@student.kuleuven.be>
2024-04-06 13:33:15 +0200

    Update sample size script

--- Sample_size.R
+++ Sample_size.R
@@ -1,16 +1,17 @@
 
+# Import the data
 G17.pilot.data <- read.csv("G17.pilot.data.csv")
 
+# Calculate SD
 sd(G17.pilot.data$tot.vase.days)
 
+# Use power.t.test function, as we have big sample size, so normal distribution can be assumed
 power.t.test(n = , delta = 1, sig.level = 0.05, sd = 4, power = 0.8, type = "two.sample",
-alternative = "one.sided", strict = TRUE)
+alternative = "one.sided", strict = TRUE) # 199
 
 power.t.test(n = , delta = 1, sig.level = 0.05, sd = 4, power = 0.9, type = "two.sample",
-             alternative = "one.sided", strict = TRUE)
+             alternative = "one.sided", strict = TRUE) # 275
 
+# The total sample size is the product of 1 compound with 15
 sample_size <- 199 * 15
-
-hist(G17.pilot.data$tot.vase.days)
-
 
